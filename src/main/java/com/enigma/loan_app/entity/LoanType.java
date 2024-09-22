5557d1d754dbffebcb6f3566b999ce8520ae4dfa
MUHAMAD_AFRIZAL FAUZI <muhamadafrizalf@gmail.com>
2024-09-22 15:17:57 +0700

    [IMPROVE] add validation and error handling on loan type service

--- src/main/java/com/enigma/loan_app/entity/LoanType.java
+++ src/main/java/com/enigma/loan_app/entity/LoanType.java
@@ -1,7 +1,10 @@
 package com.enigma.loan_app.entity;
 
+import com.enigma.loan_app.constant.Message;
 import com.enigma.loan_app.constant.PathDb;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,9 +21,12 @@ public class LoanType {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    @NotBlank(message = Message.TYPE_IS_REQUIRED)
     @Column
     private String type;
 
+    @NotBlank(message = Message.MAX_LOAN_IS_REQUIRED)
+    @Positive(message = Message.MAX_LOAN_MUST_POSITIVE)
     @Column(name = "max_loan")
     private Double maxLoan;
 }
--- src/main/java/com/enigma/loan_app/service/impl/LoanTypeServiceImpl.java
+++ src/main/java/com/enigma/loan_app/service/impl/LoanTypeServiceImpl.java
@@ -1,8 +1,11 @@
 package com.enigma.loan_app.service.impl;
 
+import com.enigma.loan_app.constant.Message;
 import com.enigma.loan_app.entity.LoanType;
 import com.enigma.loan_app.repository.LoanTypeRepository;
 import com.enigma.loan_app.service.LoanTypeService;
+import com.enigma.loan_app.util.ValidationUtil;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,32 +17,47 @@ import java.util.List;
 @RequiredArgsConstructor
 public class LoanTypeServiceImpl implements LoanTypeService {
     private final LoanTypeRepository loanTypeRepository;
+    private final ValidationUtil validationUtil;
 
+    private LoanType getLoanTypeOrThrow(String id) {
+        if (id == null || id.isEmpty()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, Message.LOAN_TYPE_ID_IS_EMPTY);
+        return loanTypeRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, Message.LOAN_TYPE_NOT_FOUND));
+    }
+
+    @Transactional(rollbackOn = Exception.class)
     @Override
     public LoanType create(LoanType loanType) {
+        validationUtil.validate(loanType);
         return loanTypeRepository.saveAndFlush(loanType);
     }
 
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
     @Override
     public LoanType findById(String id) {
-        if (id == null || id.isEmpty()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Loan type id cannot be null or empty");
-        return loanTypeRepository.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Loan type not found"));
+        return getLoanTypeOrThrow(id);
     }
 
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
     @Override
     public List<LoanType> findAll() {
         return loanTypeRepository.findAll();
     }
 
+    @Transactional(rollbackOn = Exception.class)
     @Override
     public LoanType update(LoanType loanType) {
-        findById(loanType.getId());
-        return create(loanType);
+        validationUtil.validate(loanType);
+        LoanType updatedLoanType = getLoanTypeOrThrow(loanType.getId());
+        updatedLoanType.setType(loanType.getType());
+        updatedLoanType.setMaxLoan(loanType.getMaxLoan());
+        return loanTypeRepository.saveAndFlush(updatedLoanType);
     }
 
+    @Transactional(rollbackOn = Exception.class)
     @Override
     public void delete(String id) {
-        LoanType loanType = findById(id);
-        loanTypeRepository.delete(loanType);
+        getLoanTypeOrThrow(id);
+        loanTypeRepository.deleteById(id);
     }
 }
